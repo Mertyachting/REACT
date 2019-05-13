b4fbb823ac61696791866e11e74e80ba0975252a
Mert Sevindik <mryachting@outlook.de>
2019-05-13 23:25:34 +0200

    first changes on the src/app.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    var name;
+    var surname;
+    var helloWorld = "This is the start of a great Journey";
+    var wasGeht = 'Was geht ab?';
+    return(name
+    );
+  }
+}
+
+export default App;
