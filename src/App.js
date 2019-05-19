4d24511c42a71acc8934669cf7e98e9a1fd00d7a
Mert Sevindik <mryachting@outlook.de>
2019-05-19 20:33:47 +0200

    added dismiss and delete button practice state and update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const list = [
     url: 'https://github.com/reactjs/redux',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
-    points: 5,
+    points: 9,
     objectID: 1,
   },
 ];
@@ -44,36 +44,68 @@ const listItems = numbers.map((number)=>
 <li>{number}</li>);
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      list:list,
+      games:games,
+    };
+    this.onDismiss = this.onDismiss.bind(this)
+    this.delete = this.delete.bind(this);
+  }
+
+  onDismiss(id){
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list: updatedList});
+    };
+  
+    delete(usk){
+      const isNotUsk = game => game.usk != usk;
+      const updatedUsk = this.state.games.filter(isNotUsk);
+      this.setState({games: updatedUsk});
+    }
+    
   render(){
     return(
       <div className="App">
-      {list.map(function(item){
-        return (
-        <div>
+
+
+      {this.state.list.map(item =>
+       
+       <div key={item.objectID}>
+        
           <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
-            <li>Object ID : {item.objectID}</li>
-            </div> 
-        )})}
-        {games.map(function(game){
-          return (
+        <button onClick = {() => this.onDismiss(item.objectID)} type = 'button'> Dismiss </button>
+        </div>
+      )}
+      
+
+        {this.state.games.map (game=>
+         
             <div>
               <a href={game.url}><h1>{game.title}</h1></a>
-             
+                <li>Name is {game.title.length} char long</li>
                 <li>Genre: {game.genre}</li>
                 <li>Rating: {game.usk}</li>
+                <button onClick = {() => this.delete(game.usk)} type = 'button'>delete</button>
               </div>
-          )
-        }
+          
         )}
-
         </div>
-    );
-  }
-}
+    )
+        }
+      }
+    
+        
+    
   
+
+  
+
 
  
 
